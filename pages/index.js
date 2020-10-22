5d5aa0e09d1546d31ad0d95f578fd7579d2f79e6
David Leonard <david.leonard@servicenow.com>
2020-10-22 13:03:21 -0700

    Fix homepage

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,17 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import React, {Fragment} from 'react';
+import PageHeader from '../components/PageHeader';
+import Page from '../components/Page';
 
 export default function Home() {
+	const path = [];
+	const selectedPath = 'home';
+
 	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Create Next App</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-
-			<main className={styles.main}>
-				<h1 className={styles.title}>
-					Welcome to <a href="https://nextjs.org">Next.js!</a>
-				</h1>
-
-				<p className={styles.description}>
-					Get started by editing{' '}
-					<code className={styles.code}>pages/index.js</code>
-				</p>
-
-				<div className={styles.grid}>
-					<a href="https://nextjs.org/docs" className={styles.card}>
-						<h3>Documentation &rarr;</h3>
-						<p>Find in-depth information about Next.js features and API.</p>
-					</a>
-
-					<a href="https://nextjs.org/learn" className={styles.card}>
-						<h3>Learn &rarr;</h3>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
-					</a>
-
-					<a
-						href="https://github.com/vercel/next.js/tree/master/examples"
-						className={styles.card}>
-						<h3>Examples &rarr;</h3>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
-
-					<a
-						href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className={styles.card}>
-						<h3>Deploy &rarr;</h3>
-						<p>
-							Instantly deploy your Next.js site to a public URL with Vercel.
-						</p>
-					</a>
-				</div>
-			</main>
-
-			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer">
-					Powered by{' '}
-					<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-				</a>
-			</footer>
-		</div>
+		<Fragment>
+			<PageHeader label="Theme Tools" path={path} selectedPath={selectedPath} />
+			<Page>
+				<p>Click one of the links in the header above to use the tools.</p>
+			</Page>
+		</Fragment>
 	);
 }
