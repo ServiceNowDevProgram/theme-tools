8db8d3c9deefec5083bf117e90ac4bec02112a72
David Leonard <david.leonard@servicenow.com>
2020-10-28 17:05:02 -0700

    Reformat with prettier

--- pages/color-generator/generateColors.js
+++ pages/color-generator/generateColors.js
@@ -1,27 +1,30 @@
 class Color {
-  constructor(color, name) {
-    this.hex = color;
-    this.rgb = hexToRgb(color);
-    this.name = name;
-  }
+	constructor(color, name) {
+		this.hex = color;
+		this.rgb = hexToRgb(color);
+		this.name = name;
+	}
 }
 
 const hex = (color) => {
-	  const colorString = color.toString(16);
-	  return colorString.length === 1 ? `0${colorString}` : colorString;
+	const colorString = color.toString(16);
+	return colorString.length === 1 ? `0${colorString}` : colorString;
 };
 
 const calculateColor = (color1, color2, ratio) => {
 	ratio = (Math.round(ratio * 100) / 100).toFixed(2);
 
 	const r = Math.ceil(
-		parseInt(color2.substring(0, 2), 16) * ratio + parseInt(color1.substring(0, 2), 16) * (1 - ratio),
+		parseInt(color2.substring(0, 2), 16) * ratio +
+			parseInt(color1.substring(0, 2), 16) * (1 - ratio)
 	);
 	const g = Math.ceil(
-		parseInt(color2.substring(2, 4), 16) * ratio + parseInt(color1.substring(2, 4), 16) * (1 - ratio),
+		parseInt(color2.substring(2, 4), 16) * ratio +
+			parseInt(color1.substring(2, 4), 16) * (1 - ratio)
 	);
 	const b = Math.ceil(
-		parseInt(color2.substring(4, 6), 16) * ratio + parseInt(color1.substring(4, 6), 16) * (1 - ratio),
+		parseInt(color2.substring(4, 6), 16) * ratio +
+			parseInt(color1.substring(4, 6), 16) * (1 - ratio)
 	);
 
 	return hex(r) + hex(g) + hex(b);
@@ -33,41 +36,45 @@ const hexToRgb = (hex) => {
 	const b = parseInt(hex.substring(5, 7), 16);
 
 	return `${r},${g},${b}`;
-}
-
+};
 
 const generateColors = (color1, color2, variations) => {
-  if (!variations) {
-    return [];
-  }
+	if (!variations) {
+		return [];
+	}
 
 	color1 = color1.startsWith('#') ? color1 : `#${color1}`;
 	color2 = color2.startsWith('#') ? color2 : `#${color2}`;
-  let percentage = 1/variations;
-  const numberOfLoops = 1/percentage - 1;
+	let percentage = 1 / variations;
+	const numberOfLoops = 1 / percentage - 1;
 
 	let colors = [];
 
 	for (let i = 1; i <= numberOfLoops; i++) {
-		colors = [...colors, '#' + calculateColor(color1.slice(1), color2.slice(1), i * percentage)];
+		colors = [
+			...colors,
+			'#' + calculateColor(color1.slice(1), color2.slice(1), i * percentage),
+		];
 	}
 
 	return colors;
 };
 
-
 const generateColorRange = (
-		color,
-		lightVariations,
-		lightPercentage,
-		darkVariations,
-		darkPercentage,
-		hook,
-		startIndex,
-		includeEnds,
-	) => {
-
-	const firstColor = calculateColor('ffffff', color.slice(1), 1 - lightPercentage);
+	color,
+	lightVariations,
+	lightPercentage,
+	darkVariations,
+	darkPercentage,
+	hook,
+	startIndex,
+	includeEnds
+) => {
+	const firstColor = calculateColor(
+		'ffffff',
+		color.slice(1),
+		1 - lightPercentage
+	);
 	const lastColor = calculateColor(color.slice(1), '000000', darkPercentage);
 	const lightColors = generateColors(firstColor, color, lightVariations);
 	const darkColors = generateColors(color, lastColor, darkVariations);
@@ -84,8 +91,8 @@ const generateColorRange = (
 	}
 
 	if (includeEnds) {
-		out = [`#ffffff`, ...out, `#000000`]
-  }
+		out = [`#ffffff`, ...out, `#000000`];
+	}
 
 	return out.map((color, index) => {
 		const name = `--${hook}-${index + (startIndex ? startIndex : 0)}`;
@@ -93,81 +100,90 @@ const generateColorRange = (
 	});
 };
 
-export const getColors= (selectedColorGroup, selectedColorId, colorsJson) => {
-  let colors;
-  const {
-    lightVariations,
-    lightPercentage,
-    darkVariations,
-    darkPercentage,
-    hook,
-    startIndex,
-    baseColor
-  } = colorsJson[selectedColorGroup].colors[selectedColorId];
+export const getColors = (selectedColorGroup, selectedColorId, colorsJson) => {
+	let colors;
+	const {
+		lightVariations,
+		lightPercentage,
+		darkVariations,
+		darkPercentage,
+		hook,
+		startIndex,
+		baseColor,
+	} = colorsJson[selectedColorGroup].colors[selectedColorId];
 
-  if (baseColor) {
-    colors = generateColorRange(
-      baseColor,
-      lightVariations,
-      lightPercentage,
-      darkVariations,
-      darkPercentage,
-      hook,
-      startIndex,
-      selectedColorId === 'neutrals' ? true : false
-    );
-  }
+	if (baseColor) {
+		colors = generateColorRange(
+			baseColor,
+			lightVariations,
+			lightPercentage,
+			darkVariations,
+			darkPercentage,
+			hook,
+			startIndex,
+			selectedColorId === 'neutrals' ? true : false
+		);
+	}
 
-  // SURFACE-NEUTRAL AND SURFACE-DIVIDER ARE COLORS BASED OFF THE NEUTRAL PALETTE
-  // SURFACE-NEUTRAL'S BASE COLOR IS NEUTRAL-4
-  // SURFACE-DIVIDER'S BASE COLOR IS NEUTRAL-8
-  if (colorsJson.base.colors.neutrals && colorsJson.base.colors.neutrals.baseColor) {
-    if (selectedColorId === 'surfaceNeutral' || selectedColorId === 'surfaceDivider') {
-      const {
-        lightVariations: _lightVariations,
-        lightPercentage: _lightPercentage,
-        darkVariations: _darkVariations,
-        darkPercentage: _darkPercentage,
-        hook: _hook,
-        startIndex: _startIndex,
-        baseColor: _baseColor
-      } = colorsJson.base.colors.neutrals;
+	// SURFACE-NEUTRAL AND SURFACE-DIVIDER ARE COLORS BASED OFF THE NEUTRAL PALETTE
+	// SURFACE-NEUTRAL'S BASE COLOR IS NEUTRAL-4
+	// SURFACE-DIVIDER'S BASE COLOR IS NEUTRAL-8
+	if (
+		colorsJson.base.colors.neutrals &&
+		colorsJson.base.colors.neutrals.baseColor
+	) {
+		if (
+			selectedColorId === 'surfaceNeutral' ||
+			selectedColorId === 'surfaceDivider'
+		) {
+			const {
+				lightVariations: _lightVariations,
+				lightPercentage: _lightPercentage,
+				darkVariations: _darkVariations,
+				darkPercentage: _darkPercentage,
+				hook: _hook,
+				startIndex: _startIndex,
+				baseColor: _baseColor,
+			} = colorsJson.base.colors.neutrals;
 
-      const neutralColors = generateColorRange(
-        _baseColor,
-        _lightVariations,
-        _lightPercentage,
-        _darkVariations,
-        _darkPercentage,
-        _hook,
-        _startIndex,
-        true
-      );
+			const neutralColors = generateColorRange(
+				_baseColor,
+				_lightVariations,
+				_lightPercentage,
+				_darkVariations,
+				_darkPercentage,
+				_hook,
+				_startIndex,
+				true
+			);
 
-      const {
-        lightVariations,
-        lightPercentage,
-        darkVariations,
-        darkPercentage,
-        hook,
-        startIndex
-      } = colorsJson[selectedColorGroup].colors[selectedColorId];
+			const {
+				lightVariations,
+				lightPercentage,
+				darkVariations,
+				darkPercentage,
+				hook,
+				startIndex,
+			} = colorsJson[selectedColorGroup].colors[selectedColorId];
 
-      const newBase = selectedColorId === 'surfaceNeutral' ? neutralColors[4].hex : neutralColors[8].hex;
+			const newBase =
+				selectedColorId === 'surfaceNeutral'
+					? neutralColors[4].hex
+					: neutralColors[8].hex;
 
-      colors = generateColorRange(
-        newBase,
-        lightVariations,
-        lightPercentage,
-        darkVariations,
-        darkPercentage,
-        hook,
-        startIndex,
-        false
-      );
-      colors.pop();
-    }
-  }
+			colors = generateColorRange(
+				newBase,
+				lightVariations,
+				lightPercentage,
+				darkVariations,
+				darkPercentage,
+				hook,
+				startIndex,
+				false
+			);
+			colors.pop();
+		}
+	}
 
-  return colors;
-}
+	return colors;
+};
--- pages/color-generator/index.js
+++ pages/color-generator/index.js
@@ -1,212 +1,219 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component, Fragment} from 'react';
 
 import COLORS from './colors.json';
 import PageHeader from '../../components/PageHeader';
 import Page from '../../components/Page';
-import { getColors } from './generateColors';
+import {getColors} from './generateColors';
 import cx from '../../lib/cx';
 
-const path = [{id: 'colorGenerator', href: '/color-generator', label: 'Color Generator'}];
+const path = [
+	{id: 'colorGenerator', href: '/color-generator', label: 'Color Generator'},
+];
 const selectedPath = 'color-generator';
 
 class ColorGenerator extends Component {
-  constructor(props){
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      colorsJson: JSON.parse(JSON.stringify(COLORS)),
-      selectedColorGroup: 'base'
-    };
-  }
+		this.state = {
+			colorsJson: JSON.parse(JSON.stringify(COLORS)),
+			selectedColorGroup: 'base',
+		};
+	}
 
-  componentDidMount() {
-    const { colorsJson } = this.state;
-    const mapper = {};
-    const out = {...colorsJson};
-    const urlParams = new URLSearchParams(window.location.search);
-    const entries = urlParams.entries();
+	componentDidMount() {
+		const {colorsJson} = this.state;
+		const mapper = {};
+		const out = {...colorsJson};
+		const urlParams = new URLSearchParams(window.location.search);
+		const entries = urlParams.entries();
 
-    // TAKE COLORS FROM URL SEARCH PARAMS
-    if (entries) {
-      for(const entry of entries) {
-        mapper[entry[0]] = entry[1];
-      }
+		// TAKE COLORS FROM URL SEARCH PARAMS
+		if (entries) {
+			for (const entry of entries) {
+				mapper[entry[0]] = entry[1];
+			}
 
-      if (Object.keys(mapper).length) {
-        for (const i in out) {
-          for (const j in out[i].colors) {
-            if (mapper[j] && mapper[j].length === 7) {
-              out[i].colors[j] = {
-                ...out[i].colors[j],
-                baseColor: mapper[j],
-              };
-            }
-          }
-        }
+			if (Object.keys(mapper).length) {
+				for (const i in out) {
+					for (const j in out[i].colors) {
+						if (mapper[j] && mapper[j].length === 7) {
+							out[i].colors[j] = {
+								...out[i].colors[j],
+								baseColor: mapper[j],
+							};
+						}
+					}
+				}
 
-        this.setState({
-          colorsJson: out,
-        });
-      }
-    }
-  }
+				this.setState({
+					colorsJson: out,
+				});
+			}
+		}
+	}
 
-  copyColors = () => {
-    const { colorsJson } = this.state;
-    const out = {};
+	copyColors = () => {
+		const {colorsJson} = this.state;
+		const out = {};
 
-    for (const i in colorsJson) {
-      for (const j in colorsJson[i].colors) {
-        const colors = getColors(i, j, colorsJson);
+		for (const i in colorsJson) {
+			for (const j in colorsJson[i].colors) {
+				const colors = getColors(i, j, colorsJson);
 
-        if (colors && colors.length) {
-          colors.forEach(color => {
-            out[color.name] = color.rgb;
-          });
-        }
-      }
-    }
+				if (colors && colors.length) {
+					colors.forEach((color) => {
+						out[color.name] = color.rgb;
+					});
+				}
+			}
+		}
 
-    var aux = document.createElement("input");
-    aux.setAttribute("value", JSON.stringify(out, null, 4));
-    document.body.appendChild(aux);
-    aux.select();
-    document.execCommand("copy");
-    document.body.removeChild(aux);
-    console.log('copied', JSON.stringify(out, null, 4));
-  };
+		var aux = document.createElement('input');
+		aux.setAttribute('value', JSON.stringify(out, null, 4));
+		document.body.appendChild(aux);
+		aux.select();
+		document.execCommand('copy');
+		document.body.removeChild(aux);
+		console.log('copied', JSON.stringify(out, null, 4));
+	};
 
-  renderTabs = (tabs = []) => {
-    return tabs.map((tab, i) => {
-      return (
-        <button
-          key={tab.id}
-          onClick={() => this.setState({selectedColorGroup: tab.id})}
-          className={cx({
-            'ml-8': i !== 0,
-            'font-medium': true,
-            'text-gray-500': true,
-            'hover:text-gray-900': true,
-            'transition': true,
-            'duration-150': true,
-            'ease-in-out': true
-          })}>
-            {tab.label}
-        </button>
-      );
-    });
-  }
+	renderTabs = (tabs = []) => {
+		return tabs.map((tab, i) => {
+			return (
+				<button
+					key={tab.id}
+					onClick={() => this.setState({selectedColorGroup: tab.id})}
+					className={cx({
+						'ml-8': i !== 0,
+						'font-medium': true,
+						'text-gray-500': true,
+						'hover:text-gray-900': true,
+						transition: true,
+						'duration-150': true,
+						'ease-in-out': true,
+					})}>
+					{tab.label}
+				</button>
+			);
+		});
+	};
 
-  updateUrl = (key, value) => {
-    if (history.replaceState && value) {
-      let searchParams = new URLSearchParams(window.location.search);
-      searchParams.set(key, value);
-      let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + searchParams.toString();
-      window.history.replaceState({path: newurl}, '', newurl);
-    }
-  };
+	updateUrl = (key, value) => {
+		if (history.replaceState && value) {
+			let searchParams = new URLSearchParams(window.location.search);
+			searchParams.set(key, value);
+			let newurl =
+				window.location.protocol +
+				'//' +
+				window.location.host +
+				window.location.pathname +
+				'?' +
+				searchParams.toString();
+			window.history.replaceState({path: newurl}, '', newurl);
+		}
+	};
 
-  updateBaseColor = (id, value) => {
-    const { colorsJson, selectedColorGroup } = this.state;
-    this.updateUrl(id, value);
+	updateBaseColor = (id, value) => {
+		const {colorsJson, selectedColorGroup} = this.state;
+		this.updateUrl(id, value);
 
-    this.setState({
-      colorsJson: {
-        ...colorsJson,
-        [selectedColorGroup]: {
-          ...colorsJson[selectedColorGroup],
-          colors: {
-            ...colorsJson[selectedColorGroup].colors,
-            [id]: {
-              ...colorsJson[selectedColorGroup].colors[id],
-              baseColor: value
-            }
-          }
-        }
-      }
-    });
-  }
+		this.setState({
+			colorsJson: {
+				...colorsJson,
+				[selectedColorGroup]: {
+					...colorsJson[selectedColorGroup],
+					colors: {
+						...colorsJson[selectedColorGroup].colors,
+						[id]: {
+							...colorsJson[selectedColorGroup].colors[id],
+							baseColor: value,
+						},
+					},
+				},
+			},
+		});
+	};
 
-  renderGeneratedColors = id => {
-    let colors = [];
-    const { colorsJson, selectedColorGroup } = this.state;
-    const color = colorsJson[selectedColorGroup].colors[id];
-    if (
-      (color &&
-      color.baseColor &&
-      color.baseColor.length === 7) ||
-      (colorsJson.base.colors.neutrals.baseColor &&
-      (id === 'surfaceNeutral' ||
-      id === 'surfaceDivider'))
-    ) {
-      colors = getColors(selectedColorGroup, id, colorsJson);
-    }
+	renderGeneratedColors = (id) => {
+		let colors = [];
+		const {colorsJson, selectedColorGroup} = this.state;
+		const color = colorsJson[selectedColorGroup].colors[id];
+		if (
+			(color && color.baseColor && color.baseColor.length === 7) ||
+			(colorsJson.base.colors.neutrals.baseColor &&
+				(id === 'surfaceNeutral' || id === 'surfaceDivider'))
+		) {
+			colors = getColors(selectedColorGroup, id, colorsJson);
+		}
 
-    if (colors) {
-      return colors.map((color) => {
-        return (
-          <div className="flex-1" title={color.name} key={color.name}>
-            <div style={{height: '80px', backgroundColor: color.hex}}></div>
-            <small style={{fontSize: '10px'}}>{color.hex}</small>
-          </div>
-        );
-      });
-    }
+		if (colors) {
+			return colors.map((color) => {
+				return (
+					<div className="flex-1" title={color.name} key={color.name}>
+						<div style={{height: '80px', backgroundColor: color.hex}}></div>
+						<small style={{fontSize: '10px'}}>{color.hex}</small>
+					</div>
+				);
+			});
+		}
+	};
 
-  };
+	renderColorGroups = () => {
+		const {colorsJson, selectedColorGroup} = this.state;
+		if (colorsJson[selectedColorGroup]) {
+			return Object.values(colorsJson[selectedColorGroup].colors).map(
+				(group) => {
+					const {id, label, baseColor} = group;
+					const isNeutral = id === 'surfaceNeutral' || id === 'surfaceDivider';
+					return (
+						<div key={id} className="mb-6">
+							<label className="block text-sm leading-5 font-medium text-gray-700">{`${label}  ${
+								isNeutral ? '(generated from neutrals)' : ''
+							}`}</label>
+							{!isNeutral ? (
+								<input
+									className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 mb-2"
+									type="text"
+									name={id}
+									value={baseColor || ''}
+									onChange={(e) => {
+										this.updateBaseColor(id, e.target.value || undefined);
+									}}
+									placeholder={`#000000`}
+								/>
+							) : null}
 
-  renderColorGroups = () => {
-    const { colorsJson, selectedColorGroup } = this.state;
-    if (colorsJson[selectedColorGroup]) {
-      return Object.values(colorsJson[selectedColorGroup].colors).map(group => {
-        const { id, label, baseColor } = group;
-        const isNeutral = id === 'surfaceNeutral' || id === 'surfaceDivider';
-        return (
-          <div key={id} className="mb-6">
-            <label className="block text-sm leading-5 font-medium text-gray-700">{`${label}  ${isNeutral ? '(generated from neutrals)' : ''}`}</label>
-              {!isNeutral ?
-                <input
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 mb-2"
-                  type="text"
-                  name={id}
-                  value={baseColor || ''}
-                  onChange={(e) => {
-                    this.updateBaseColor(id, e.target.value || undefined);
-                  }}
-                  placeholder={`#000000`}
-                /> : null}
+							<div className="flex">{this.renderGeneratedColors(id)}</div>
+						</div>
+					);
+				}
+			);
+		}
+	};
 
-            <div className="flex">
-              {this.renderGeneratedColors(id)}
-            </div>
-          </div>
-        );
-      });
-    }
-  };
-
-  render() {
-    return (
-      <Fragment>
-        <PageHeader label="Color Generator" path={path} selectedPath={selectedPath} />
-        <Page wide>
-          <div className="mb-8 flex justify-between items-center">
-            <div>
-              {this.renderTabs(Object.values(this.state.colorsJson))}
-            </div>
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded ml-auto"
-              onClick={this.copyColors}>
-              Copy Json
-            </button>
-          </div>
-          <div>
-            {this.renderColorGroups()}
-          </div>
-        </Page>
-      </Fragment>
-    );
-  }
+	render() {
+		return (
+			<Fragment>
+				<PageHeader
+					label="Color Generator"
+					path={path}
+					selectedPath={selectedPath}
+				/>
+				<Page wide>
+					<div className="mb-8 flex justify-between items-center">
+						<div>{this.renderTabs(Object.values(this.state.colorsJson))}</div>
+						<button
+							className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded ml-auto"
+							onClick={this.copyColors}>
+							Copy Json
+						</button>
+					</div>
+					<div>{this.renderColorGroups()}</div>
+				</Page>
+			</Fragment>
+		);
+	}
 }
 
 export default ColorGenerator;
