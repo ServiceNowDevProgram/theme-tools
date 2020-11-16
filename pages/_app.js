631a3e33bdc3f9ad0fa7df2ad2c9c355a61202da
bryant.yu <bryant.yu@servicenow.com>
2020-11-16 10:39:29 -0800

    added toaster and copy json btn

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
+import React, {Fragment} from 'react';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/index.css';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 import 'tippy.js/dist/tippy.css';
 
 function MyApp({Component, pageProps}) {
-	return <Component {...pageProps} />;
+	return (
+		<Fragment>
+			<Component {...pageProps} />
+			<ToastContainer />
+		</Fragment>
+	);
 }
 
 export default MyApp;
