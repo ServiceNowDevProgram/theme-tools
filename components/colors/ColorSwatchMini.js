d25a341f36809c331cafb741904c2f6a6c755a68
David Leonard <david.leonard@servicenow.com>
2021-07-08 15:31:18 -0700

    Refactor color info popup into own component

--- components/colors/ColorSwatchMini.js
+++ components/colors/ColorSwatchMini.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CopyValue from '../CopyValue';
-import Tippy from '@tippyjs/react';
 import {getContrastRatio} from '../../lib/color-generator/generateColors';
+import ColorInfoPopup from './ColorInfoPopup';
 
 function getOnColor(baseColor) {
 	const contrastWhite = getContrastRatio(baseColor, '#ffffff');
@@ -13,67 +13,27 @@ function getOnColor(baseColor) {
 	}
 }
 
-function ColorCircle({color}) {
-	const tooltip = (
-		<div>
-			<div className="w-full h-20" style={{backgroundColor: color.hex}}></div>
-			<div className="p-4 border-t border-gray-500">
-				<p className="text-2xs uppercase opacity-75 pb-1">SCSS Variable</p>
-				<CopyValue withButton value={color.scssVariable}>
-					<p>{color.scssVariable}</p>
-				</CopyValue>
-
-				<p className="text-2xs uppercase opacity-75 pb-1 mt-3">
-					CSS Custom Property
-				</p>
-				<CopyValue withButton value={color.customProperty}>
-					<p>{color.customProperty}</p>
-				</CopyValue>
-
-				<p className="text-2xs uppercase opacity-75 pb-1 mt-3">Hex Color</p>
-				<CopyValue withButton value={color.hex}>
-					<p>{color.hex}</p>
-				</CopyValue>
-
-				<p className="text-2xs uppercase opacity-75 pb-1 mt-3">RGB Color</p>
-				<CopyValue withButton value={color.rgb}>
-					<p>{color.rgb}</p>
-				</CopyValue>
-			</div>
-		</div>
-	);
-
-	return (
-		<Tippy
-			content={tooltip}
-			trigger="click"
-			interactive={true}
-			theme="colorbox"
-			arrow={false}>
-			<button
-				style={{
-					height: '45px',
-					width: '45px',
-					marginRight: '-8px',
-					borderRadius: '100%',
-					border: '1px solid lightgray',
-					display: 'flex',
-					alignItems: 'center',
-					justifyContent: 'center',
-					backgroundColor: color.hex,
-					color: getOnColor(color.hex),
-				}}>
-				<span className="text-xs">{color.index}</span>
-			</button>
-		</Tippy>
-	);
-}
-
 const ColorSwatchMini = ({items}) => {
 	return (
 		<div className="flex flex-wrap">
-			{items.map((color, index) => (
-				<ColorCircle color={color} index={index} key={color.id} />
+			{items.map((color) => (
+				<ColorInfoPopup color={color} key={color.id}>
+					<button
+						style={{
+							height: '45px',
+							width: '45px',
+							marginRight: '-8px',
+							borderRadius: '100%',
+							border: '1px solid lightgray',
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'center',
+							backgroundColor: color.hex,
+							color: getOnColor(color.hex),
+						}}>
+						<span className="text-xs">{color.index}</span>
+					</button>
+				</ColorInfoPopup>
 			))}
 		</div>
 	);
@@ -83,10 +43,11 @@ ColorSwatchMini.propTypes = {
 	items: PropTypes.arrayOf(
 		PropTypes.shape({
 			id: PropTypes.string,
-			name: PropTypes.string,
 			hex: PropTypes.string,
+			rgb: PropTypes.string,
 			customProperty: PropTypes.string,
 			scssVariable: PropTypes.string,
+			index: PropTypes.number,
 		})
 	),
 };
