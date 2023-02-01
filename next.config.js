1f33b852be682a2d00ec895dd1a08cd778c0183e
nolan.seaton <nolan.seaton@servicenow.com>
2023-02-01 10:23:29 -0800

    Update base path for gh pages

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const dev = process.env.NODE_ENV !== 'production';
 const dokku = process.env.DEPLOY_TARGET === 'dokku';
 
 module.exports = {
-	basePath: !dev && !dokku ? '/pages/ServiceNowDevProgram/theme-tools/out' : '',
+	basePath: !dev && !dokku ? '/theme-tools' : '',
 	trailingSlash: dokku ? true : undefined,
 	async redirects() {
 		return [
