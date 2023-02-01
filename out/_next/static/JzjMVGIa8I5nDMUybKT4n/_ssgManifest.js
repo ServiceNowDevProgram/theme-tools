befb97c9c0f78202f0a72f19852f9919dfc6f2aa
nolan.seaton <nolan.seaton@servicenow.com>
2023-02-01 09:52:37 -0800

    Build for pages

--- out/_next/static/JzjMVGIa8I5nDMUybKT4n/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fschemas","\u002Freleases","\u002Fvalidator","\u002Fschema\u002F[id]","\u002Frelease\u002F[id]","\u002Fhook\u002F[uid]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
